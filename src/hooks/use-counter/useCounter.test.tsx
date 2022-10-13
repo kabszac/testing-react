3d3b96eae305cf579fa2ba8d9b7e47f890c197f8
Isaac Kabucho <kabszac@gmail.com>
2022-10-13 05:49:56 +0300

    learnt how to use act utility to  ensures updates are processed before assertions are made

--- src/hooks/use-counter/useCounter.test.tsx
+++ src/hooks/use-counter/useCounter.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react'
+import { renderHook, act } from '@testing-library/react'
 import { useCounter } from './useCounter'
 
 describe('useCounter', () => {
@@ -15,4 +15,20 @@ describe('useCounter', () => {
         })
         expect(result.current.count).toBe(10)
     })
+
+    test('Should increment the count', () => {
+        const {result} = renderHook(useCounter)
+        act(() => result.current.increment())
+        expect(result.current.count).toBe(1)
+    })
+
+    test('Should decrement the count', () => {
+        const {result} = renderHook(useCounter, {
+            initialProps:{
+                initialCount: 5
+            }
+        })
+        act(() => result.current.decrement())
+        expect(result.current.count).toBe(4)
+    }) 
 })
